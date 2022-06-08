7a4b2306eff63fef2ef8f39cac495598f723d6e5
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-08 13:30:31 +0900

    回転

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -60,28 +60,34 @@ void GameScene::Initialize() {
 	                                     0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 	//各軸の回転行列の要素数を設定
 	matRotZ = {
-
+		cos(worldTransform_.rotation_.z), sin(worldTransform_.rotation_.z),0.0f,0.0f,
+	  -sin(worldTransform_.rotation_.z), cos(worldTransform_.rotation_.z), 0.0f, 0.0f,
+	  0.0f,0.0f,1.0f,0.0f,
+	  0.0f,0.0f,0.0f,1.0f
 	};
 	
 	matRotX = {
+		1.0f,0.0f,0.0f,0.0f,
+	  0.0f,cos(worldTransform_.rotation_.x),sin(worldTransform_.rotation_.x),0.0f,
+	  0.0f,-sin(worldTransform_.rotation_.x),cos(worldTransform_.rotation_.x),0.0f,
+	  0.0f,0.0f,0.0f,1.0f
 	};
 	
 	matRotY = {
+		cos(worldTransform_.rotation_.y),0.0f, -sin(worldTransform_.rotation_.y),0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+	  sin(worldTransform_.rotation_.y),0.0f,cos(worldTransform_.rotation_.y),0.0f,
+	  0.0f,0.0f,0.0f,1.0f
 	};
-	//各軸の回転行列を合成
-	matRot = matRotZ.operator*=(matRotX).operator*=(matRotY); //順番が大事
-	//平行移動行列を宣言
-	Matrix4 matTrans = MathUtility::Matrix4Identity();
-	//移動量を行列に設定
-	matTrans = MathUtility::Matrix4Translation(
-	  worldTransform_.translation_.x, worldTransform_.translation_.y,
-	  worldTransform_.translation_.z);
 
+	//各軸の回転行列を合成
+	matRot *= matRotZ *= matRotX *= matRotY; //順番が大事
+	
 	//単位行列を代入
 	worldTransform_.matWorld_ = {1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
 	                             0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 	//合成
-	worldTransform_.matWorld_ *= matScale;
+	worldTransform_.matWorld_ *= matRot;
 
 	//行列の転送
 	worldTransform_.TransferMatrix();
