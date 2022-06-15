6cda71cdfed2fe80ee214a69275b88ca1cd68f7b
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-15 15:14:09 +0900

    親動かしたい

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -24,13 +24,44 @@ void GameScene::Initialize() {
 	model_ = Model::Create();
 
 	//ワールドトランスフォームの初期化
-	//親(0番)
-	worldTransforms_[0].Initialize();
-	//子(1番)
-	worldTransforms_[1].Initialize();
-	worldTransforms_[1].translation_ = {0.0f, 4.5f, 0.0f};
-	worldTransforms_[1].parent_ = &worldTransforms_[0];
+	//キャラクターの大元
+	worldTransforms_[PartId::kRoot].Initialize();
+	//脊髄
+	worldTransforms_[PartId::kSpine].Initialize();
+	worldTransforms_[PartId::kSpine].parent_ = &worldTransforms_[PartId::kRoot];
+	worldTransforms_[PartId::kSpine].translation_ = {0.0f, 2.0f, 0.0f};
+	//上半身
+	//胸
+	worldTransforms_[PartId::kChest].Initialize();
+	worldTransforms_[PartId::kChest].parent_ = &worldTransforms_[PartId::kSpine];
+	worldTransforms_[PartId::kChest].translation_ = {0.0f, 0.0f, 0.0f};
+	//頭
+	worldTransforms_[PartId::kHead].Initialize();
+	worldTransforms_[PartId::kHead].parent_ = &worldTransforms_[PartId::kChest];
+	worldTransforms_[PartId::kHead].translation_ = {0.0f, 2.0f, 0.0f};
+	//左腕
+	worldTransforms_[PartId::kArmL].Initialize();
+	worldTransforms_[PartId::kArmL].parent_ = &worldTransforms_[PartId::kChest];
+	worldTransforms_[PartId::kArmL].translation_ = {-2.0f, 0.0f, 0.0f};
+	//右腕
+	worldTransforms_[PartId::kArmR].Initialize();
+	worldTransforms_[PartId::kArmR].parent_ = &worldTransforms_[PartId::kChest];
+	worldTransforms_[PartId::kArmR].translation_ = {2.0f, 0.0f, 0.0f};
 
+	//下半身
+	//尻
+	worldTransforms_[PartId::kHip].Initialize();
+	worldTransforms_[PartId::kHip].parent_ = &worldTransforms_[PartId::kSpine];
+	worldTransforms_[PartId::kHip].translation_ = {0.0f, -2.0f, 0.0f};
+	//左足
+	worldTransforms_[PartId::kLegL].Initialize();
+	worldTransforms_[PartId::kLegL].parent_ = &worldTransforms_[PartId::kHip];
+	worldTransforms_[PartId::kLegL].translation_ = {-2.0f, -4.0f, 0.0f};
+	//右足
+	worldTransforms_[PartId::kLegR].Initialize();
+	worldTransforms_[PartId::kLegR].parent_ = &worldTransforms_[PartId::kHip];
+	worldTransforms_[PartId::kLegR].translation_ = {2.0f, -4.0f, 0.0f};
+	
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 	//デバッグカメラの生成
@@ -48,21 +79,20 @@ void GameScene::Update() {
 	//キャラクター移動処理
 	Vector3 move = MyMathUtility::MyVector3Zero();	
 	if (input_->PushKey(DIK_RIGHT)) {
-		move.x += 0.5f;
+		move.x += 0.3f;
 	}
 	else if(input_->PushKey(DIK_LEFT)) {
-		move.x -= 0.5f;
+		move.x -= 0.3f;
 	}
 	//親の更新
-	worldTransforms_[0].translation_ += move;
-	worldTransforms_[0].matWorld_ = MyMathUtility::MyMatrix4WorldTransform(worldTransforms_[0]);
-	worldTransforms_[0].TransferMatrix();
+	worldTransforms_[PartId::kRoot].translation_ += move;
+	
+	for (size_t i = 0; i < PartId::kNumPartId; i++) {
+		worldTransforms_[i].matWorld_ = MyMathUtility::MyMatrix4WorldTransform(worldTransforms_[i]);
+		worldTransforms_[i].matWorld_ *= worldTransforms_[i].parent_->matWorld_;
+		worldTransforms_[i].TransferMatrix();
 
-	//子の更新
-	worldTransforms_[1].matWorld_ = MyMathUtility::MyMatrix4WorldTransform(worldTransforms_[1]);
-	worldTransforms_[1].matWorld_ *= worldTransforms_[0].matWorld_;
-	worldTransforms_[1].TransferMatrix();
-
+	}
 	//デバック用表示
 	debugText_->SetPos(50, 50);
 	debugText_->Printf(
@@ -85,8 +115,8 @@ void GameScene::Update() {
 
 	debugText_->SetPos(50, 150);
 	debugText_->Printf(
-	  "pos(%f,%f,%f)", worldTransforms_[0].translation_.x, worldTransforms_[0].translation_.y,
-	  worldTransforms_[0].translation_.z);
+	  "pos(%f,%f,%f)", worldTransforms_[PartId::kRoot].translation_.x, worldTransforms_[PartId::kRoot].translation_.y,
+	  worldTransforms_[PartId::kRoot].translation_.z);
 }
 
 void GameScene::Draw() {
@@ -116,9 +146,11 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	// 3Dモデル描画
-	model_->Draw(worldTransforms_[0], viewProjection_, textureHandle_);
-	model_->Draw(worldTransforms_[1], viewProjection_, textureHandle_);
-
+	for (size_t i = 0; i < PartId::kNumPartId; i++) {
+		if (i == PartId::kRoot)  continue;
+		if (i == PartId::kSpine) continue;
+		model_->Draw(worldTransforms_[i], viewProjection_, textureHandle_);
+	}
 	//ライン描画が参照するビュープロジェクションを指定する(アドレス無し)
 
 	for (int i = 0; i < 12; i++) {
