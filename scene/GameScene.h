b98615e3bd11c914619149acf8ab7f3b857adefe
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-16 13:48:53 +0900

    線の描画はいらないので消す

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -6,11 +6,11 @@
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
-#include "math/MyMathUtility.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "math/MyMathUtility.h"
 
 /// <summary>
 /// ゲームシーン
@@ -56,40 +56,12 @@ class GameScene {
 	WorldTransform worldTransforms_[100];
 	//ビュープロジェクション
 	ViewProjection viewProjection_;
+	//デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
 	//カメラ上方向の角度
 	float viewAngle = 0.0f;
 
-	//デバッグカメラ
-	DebugCamera* debugCamera_ = nullptr;
-
-	Vector3 cubeVertex_[8] = {
-	  {0.0f, 5.0f, 0.0f},
-      {0.0f, 0.0f, 0.0f},
-      {5.0f, 0.0f, 0.0f},
-      {5.0f, 5.0f, 0.0f},
-	  {0.0f, 5.0f, 5.0f},
-      {0.0f, 0.0f, 5.0f},
-      {5.0f, 0.0f, 5.0f},
-      {5.0f, 5.0f, 5.0f}
-    };
-
-	int edgeList_[12][2] = {
-	  {0, 1},
-      {1, 2},
-      {2, 3},
-      {3, 0},
-      {4, 5},
-      {5, 6},
-	  {6, 7},
-      {7, 4},
-      {0, 4},
-      {1, 5},
-      {2, 6},
-      {3, 7}
-    };
-
-	Vector4 color_ = {0xFF, 0xFF, 0xFF, 0xFF};
 	/// <summary>
-  /// ゲームシーン用
-  /// </summary>
-	};
+	/// ゲームシーン用
+	/// </summary>
+};
