1a2f98d3928ade72e4300956a72e6acd1f095514
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-08-22 04:11:16 +0900

    オプションクラス作成開始

---
+++ Player/Option.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include <cassert>
+
+//オプション(ガンダムでいうファンネルみたいな)
+class OptionBit {
+  public:
+	//初期化
+	void Initialize(Model* model);
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw(ViewProjection& viewprojection);
+
+  private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+};
